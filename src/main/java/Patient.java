a9fc5764cc90a127e65253a4f6dc06cf7fdf0f1f
eliannegolan <elianne.golan19@imperial.ac.uk>
2021-12-02 11:13:03 +0000

    new text

---
+++ src/main/java/Patient.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public class Patient implements Serializable
+{
+    protected String name;
+    protected String id;
+    protected String phoneNumber;
+
+
+}
--- src/main/java/elianne.java
+++ src/main/java/elianne.java
@@ -3,12 +3,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.stream.Collectors;
+import com.google.gson.Gson;
+
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 
-public class elianne extends HttpServlet
-{
+public class elianne extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
@@ -22,7 +27,14 @@ public class elianne extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        Gson gson = new Gson();
+        Patient p = gson.fromJson(reqBody,Patient.class);
 
+        resp.setContentType("application/json");
+        resp.getWriter().write("Thank you client!" + reqBody);
+        resp.setStatus(201);
 
     }
 }
+
